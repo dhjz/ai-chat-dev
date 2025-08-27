6a0a71e0c11a1f5fd5a0c11fa500a4f5d431a3f5
dhjz <909514844@qq.com>
2025-08-27 13:50:02 +0800

    Service worker

---
+++ sw.js
@@ -0,0 +1,35 @@
+
+const CACHE_NAME = 'chat-cache-v1';
+const URLS_TO_PRECACHE = [
+  '/',
+  // '/favicon.ico',
+  '/index.html',
+  '/setting.html',
+  '/run.html',
+  '/index-ai.css',
+  '/index-ai.js',
+  '/dist.min.js',
+];
+
+self.addEventListener('install', event => {
+  console.log(`[SW-CHAT] Event: install (version: ${CACHE_NAME})`);
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_PRECACHE))
+    .then(() => self.skipWaiting()).catch(e => console.error('[SW-CHAT] Precaching failed:', e))
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log(`[SW-CHAT] Event: activate (version: ${CACHE_NAME})`);
+  event.waitUntil(
+    caches.keys().then(cacheNames => Promise.all(cacheNames.map(cacheName => cacheName !== CACHE_NAME && caches.delete(cacheName))))
+      .then(() => self.clients.claim())
+  )
+});
+
+self.addEventListener('fetch', event => {
+  const { request } = event;
+  if (request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(request).then(cachedResponse => cachedResponse || fetch(request)).catch(() => {})
+  );
+});
